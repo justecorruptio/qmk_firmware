90658eac0bed5c8de09c45feddde7968766551f0
Jay Chan <jay@jay.vg>
2019-06-23 16:20:01 -0700

    adding numpad, cleaning up

--- keyboards/planck/keymaps/corruptio/config.h
+++ keyboards/planck/keymaps/corruptio/config.h
@@ -3,9 +3,10 @@
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
 
-    #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
-                                  SONG(COLEMAK_SOUND) \
-                                }
+    #define DEFAULT_LAYER_SONGS { \
+        SONG(QWERTY_SOUND), \
+        SONG(COLEMAK_SOUND) \
+    }
 #endif
 
 #define NO_DEBUG
