77566c8f6e49e28e927514b999a42a89477d5b60
Jay Chan <jay@jay.vg>
2019-08-06 02:32:04 -0700

    adding my gherkin

---
+++ keyboards/40percentclub/gherkin/keymaps/corruptio/keymap.c
@@ -0,0 +1,48 @@
+#include QMK_KEYBOARD_H
+
+#define RAISE LT(1, KC_SPC)
+#define LOWER LT(2, KC_BSPC)
+#define ADJUS LT(4, KC_DOT)
+
+#define t(kc) LT(3, kc)
+#define s(kc) S(kc)
+#define c(kc) MT(MOD_LCTL,kc)
+#define g(kc) MT(MOD_LGUI,kc)
+#define a(kc) MT(MOD_LALT,kc)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [0] = LAYOUT_ortho_3x10(
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+ t(KC_Z), a(KC_X), g(KC_C), c(KC_V),   LOWER,   RAISE, c(KC_N), g(KC_M), a(KC_B), t(KC_DOT)
+  ),
+
+  [1] = LAYOUT_ortho_3x10(
+    KC_1,    KC_2,    KC_3, KC_PIPE, _______, _______, _______,   KC_UP,  KC_TAB,  KC_ESC,
+    KC_4,    KC_5,    KC_6, KC_BSLS, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,
+    KC_7,    KC_8,    KC_9,    KC_0, _______, _______, KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT
+  ),
+
+  [2] = LAYOUT_ortho_3x10(
+    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  _______, KC_LPRN, KC_RPRN, KC_QUOT, KC_DQUO,
+    KC_CIRC, KC_AMPR, KC_ASTR, KC_PLUS, KC_MINS,  _______, KC_LCBR, KC_RCBR, KC_QUES, KC_SCLN,
+     KC_GRV, KC_TILD, KC_SLSH,  KC_EQL, _______,  _______, KC_LBRC, KC_RBRC,   KC_LT,   KC_GT
+  ),
+
+  [3] = LAYOUT_ortho_3x10(
+ s(KC_Q), s(KC_W), s(KC_E), s(KC_R), s(KC_T), s(KC_Y), s(KC_U), s(KC_I), s(KC_O), s(KC_P),
+ s(KC_A), s(KC_S), s(KC_D), s(KC_F), s(KC_G), s(KC_H), s(KC_J), s(KC_K), s(KC_L), KC_UNDS,
+ s(KC_Z), s(KC_X), s(KC_C), s(KC_V), _______, _______, s(KC_N), s(KC_M), s(KC_B), KC_COMM
+  ),
+
+  [4] = LAYOUT_ortho_3x10(
+      RESET, _______, _______, _______, _______, _______, _______, KC_VOLU, KC_BRID, KC_BRIU,
+    _______, _______, _______, _______, _______, _______, KC_MRWD, KC_VOLD, KC_MFFD, KC_MPLY,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  )
+};
+
+uint32_t layer_state_set_user(uint32_t state) {
+    return update_tri_layer_state(state, 1, 2, 4);
+}
--- keyboards/planck/keymaps/corruptio_split/keymap.c
+++ keyboards/planck/keymaps/corruptio_split/keymap.c
@@ -1,21 +1,8 @@
 #include QMK_KEYBOARD_H
 
 extern keymap_config_t keymap_config;
-
-enum planck_layers {
-  _QWERTY,
-  _LOL,
-  _GAME,
-  _LOWER,
-  _RAISE,
-  _ADJUST
-};
-
-enum planck_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOL,
-  GAME
-};
+enum planck_layers {_QWERTY, _LOWER, _RAISE, _LOL, _GAME, _ADJUST};
+enum planck_keycodes {QWERTY=SAFE_RANGE, LOL, GAME};
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -25,13 +12,8 @@ enum planck_keycodes {
 #define mG(kc) MT(MOD_LGUI,kc)
 #define mA(kc) MT(MOD_LALT,kc)
 
-//#define l_ESC  LT(LOWER, KC_ESC)
 #define l_BSPC  LT(LOWER, KC_BSPC)
-//#define l_TAB  LT(LOWER, KC_TAB)
-
-//#define r_TAB  LT(RAISE, KC_TAB)
-#define r_SPC  LT(RAISE, KC_SPC)
-//#define r_ENT  LT(RAISE, KC_ENT)
+#define r_SPC   LT(RAISE, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -39,7 +21,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_Q,    KC_W,    KC_E,    KC_R,mA(KC_T), XXXXXXX, XXXXXXX,mA(KC_Y),    KC_U,    KC_I,    KC_O,    KC_P,
        KC_A,    KC_S,    KC_D,    KC_F,mG(KC_G), XXXXXXX, XXXXXXX,mG(KC_H),    KC_J,    KC_K,    KC_L,  KC_ENT,
    mS(KC_Z),    KC_X,    KC_C,    KC_V,mC(KC_B), XXXXXXX, XXXXXXX,mC(KC_N),    KC_M, KC_COMM,  KC_DOT,mS(KC_QUOT),
-    XXXXXXX,  XXXXXXX, XXXXXXX, KC_ESC,  l_BSPC,   KC_NO,   KC_NO,   r_SPC,  KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX,  XXXXXXX, XXXXXXX, KC_ESC,  l_BSPC, XXXXXXX, XXXXXXX,   r_SPC,  KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX
+),
+
+[_LOWER] = LAYOUT_planck_grid(
+    _______, KC_LPRN, KC_QUES, KC_RPRN, KC_LALT, _______, _______, _______,    KC_7,    KC_8,    KC_9,    KC_0,
+    _______, KC_LCBR, KC_BSLS, KC_RCBR, KC_LGUI, _______, _______, _______,    KC_4,    KC_5,    KC_6, _______,
+    KC_LSFT, KC_LBRC, KC_SLSH, KC_RBRC, KC_LCTL, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_PIPE,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_RAISE] = LAYOUT_planck_grid(
+    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, _______, _______, KC_LALT, KC_PGUP,   KC_UP, KC_PGDN, _______,
+    KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_MINS, _______, _______, KC_LGUI, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,
+     KC_GRV, KC_TILD, KC_SLSH,  KC_EQL, KC_PLUS, _______, _______, KC_LCTL, _______, _______, _______, KC_SCLN,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_LOL] = LAYOUT_planck_grid(
@@ -54,20 +50,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F, XXXXXXX, XXXXXXX,    KC_T, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V, XXXXXXX, XXXXXXX,    KC_B, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,  KC_SPC, XXXXXXX, XXXXXXX,  KC_SPC, KC_LGUI, XXXXXXX, XXXXXXX,  QWERTY
-),
-
-[_LOWER] = LAYOUT_planck_grid(
-    _______, KC_LPRN, KC_QUES, KC_RPRN, KC_LALT, _______, _______, _______,    KC_7,    KC_8,    KC_9,    KC_0,
-    _______, KC_LCBR, KC_BSLS, KC_RCBR, KC_LGUI, _______, _______, _______,    KC_4,    KC_5,    KC_6, _______,
-    KC_LSFT, KC_LBRC, KC_SLSH, KC_RBRC, KC_LCTL, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_PIPE,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-),
-
-[_RAISE] = LAYOUT_planck_grid(
-    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, XXXXXXX, XXXXXXX, KC_LALT, _______,   KC_UP, _______, _______,
-    KC_CIRC, KC_AMPR, KC_ASTR, KC_UNDS, KC_MINS, XXXXXXX, XXXXXXX, KC_LGUI, KC_LEFT, KC_DOWN, KC_RGHT, KC_COLN,
-     KC_GRV, KC_TILD, KC_SLSH,  KC_EQL, KC_PLUS, XXXXXXX, XXXXXXX, KC_LCTL, _______, _______, _______, KC_SCLN,
-    XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_ADJUST] = LAYOUT_planck_grid(
